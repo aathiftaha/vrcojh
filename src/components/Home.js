3d1fe45ddc71d6641e2a3d7070ee983079dd6ab3
Aathif Taha <aathif.tm@gmail.com>
2023-12-05 01:05:26 +0530

    created name field and month and year

---
+++ src/components/Home.js
@@ -0,0 +1,46 @@
+import {useState} from 'react';
+import TextField from "@mui/material/TextField"
+import * as React from 'react';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+ function DatePickerViews() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DatePicker', 'DatePicker', 'DatePicker']}>
+      
+        <DatePicker label={'"month" and "year"'} views={['month', 'year']} />
+      </DemoContainer>
+    </LocalizationProvider>
+  );
+}
+
+
+export default function Home() {
+    const [name, setName] = useState('');
+
+    const onNameChange = (evt) => {
+        console.log(evt.target)
+        setName(evt.target.value)
+        }
+
+    return (
+        <div>
+            <TextField
+          error
+          id="standard-error"
+          label="Firstname"
+          defaultValue="Hello World"
+          variant="standard"
+          value={name}
+          onChange={onNameChange}
+        />
+        <div id="date-picker" style={{width:'200px'}}>
+        <DatePickerViews />
+        </div>
+        </div>
+    )
+}
+
