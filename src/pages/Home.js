263163881c015841dd0b1a3119d23af5c1db4472
Aathif Taha <aathif.tm@gmail.com>
2023-12-10 12:33:42 +0530

    code cleanup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,12 +15,13 @@ export default function Home() {
         <span>Python, SQL, Javascript, React, HTML, CSS, Tableau</span>
       </p>
       <p>
-        <a href="/path-to-resume.pdf" download style={{ margin: '30px 0' }}>
+        <a href="https://drive.google.com/file/d/1Rz_KByoYKI4FlAC-64EIU8FuGJOM2DFo/view?usp=sharing" target='_blank' style={{ margin: '30px 0' }}>
           Download Resume
         </a>
       </p>
       <br />
       <Link to="/bmi-calculator">Go to BMI Calculator</Link>
+      
     </div>
   );
 }
