3553644941d7909f57398076513bf46be267ec00
Aathif Taha <aathif.tm@gmail.com>
2023-12-10 12:03:11 +0530

    added-styles

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,9 +2,11 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import BmiPage from './pages/BmiPage';
 import Navbar from './components/Navbar';
+import './App.css';
 
 export default function AppRouter() {
   return (
+    <div className='app-container'>
     <BrowserRouter>
     <Navbar />
       <Routes>
@@ -12,5 +14,6 @@ export default function AppRouter() {
         <Route path="/bmi-calculator" element={<BmiPage />} />
       </Routes>
     </BrowserRouter>
+    </div>
   )
 }
